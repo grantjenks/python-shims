6df543240dbef5537aeaa5d63c0486924a82cb06
Grant Jenks <grant.jenks@gmail.com>
2020-06-21 23:20:13 -0700

    Basic prototype for patch() and stop()

--- shims/core.py
+++ shims/core.py
@@ -1,8 +1,18 @@
+from unittest.mock import MagicMock, patch
+
 class Shims:
+    def __init__(self):
+        self._patches = []
+
     def patch(self, target):
-        # TODO: Patch target.
-        pass
+        magic_mock = MagicMock()
+        target_path = f'{target.__module__}.{target.__qualname__}'
+        patched = patch(target_path, magic_mock)
+        patched.start()
+        self._patches.append(patched)
+        return magic_mock
 
     def stop(self):
-        # TODO: Stop all patches.
-        pass
+        for patched in self._patches:
+            patched.stop()
+        self._patches.clear()
